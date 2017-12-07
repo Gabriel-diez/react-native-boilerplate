5b1cc59a32f6d5604d3a4b6867b66c3487d02400
Diez Gabriel <diez.gabriel64@gmail.com>
2017-12-07 14:41:36 +0100

    create signForm for mutual form between login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,110 +1,13 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {
-    View,
-    Text,
-    ActivityIndicator,
-    TextInput,
-} from 'react-native';
-import {
-    loginUser,
-} from '../actions/index';
-import SubmitButton from './SubmitButton';
+import React from 'react';
+import { SignForm } from './SignForm';
 
-const styles = {
-    textInput: {
-        height: 40,
-        borderColor: 'grey',
-        borderWidth: 1,
-        marginTop: 10,
-    },
+const Login = () => {
+    return (
+        <SignForm
+            formType="login"
+        />
+    );
 };
 
-export class Login extends Component {
-    state = {
-        email: '',
-        password: '',
-    };
+export default Login;
 
-    onEmailChange = (email) => {
-        this.setState({
-            email,
-        });
-    };
-
-    onPasswordChange = (password) => {
-        this.setState({
-            password,
-        });
-    };
-
-    onButtonPress = () => {
-        const {
-            email,
-            password,
-        } = this.state;
-
-        this.props.loginUser({
-            email,
-            password,
-        });
-    };
-
-    renderButton() {
-        if (this.props.loading) {
-            return (
-                <View>
-                    <ActivityIndicator />
-                </View>
-            );
-        }
-
-        return (
-            <SubmitButton
-                onPress={this.onButtonPress}
-                submitText="Se connecter"
-            />
-        );
-    }
-
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', margin: 10 }}>
-                <Text style={{ alignSelf: 'center' }}>
-                    Connexion
-                </Text>
-                <View>
-                    <TextInput
-                        style={styles.textInput}
-                        onChangeText={this.onEmailChange}
-                        placeholder="Email"
-                        value={this.state.email}
-                    />
-
-                    <TextInput
-                        style={styles.textInput}
-                        secureTextEntry
-                        placeholder="Password"
-                        onChangeText={this.onPasswordChange}
-                        value={this.state.password}
-                    />
-                </View>
-
-                <Text>
-                    {this.props.error}
-                </Text>
-                {this.renderButton()}
-            </View>
-
-        );
-    }
-}
-
-const mapStateToProps = ({ auth }) => {
-    const { error, loading } = auth;
-    return { error, loading };
-};
-
-export default connect(mapStateToProps, {
-    loginUser,
-})(Login);
---
+++ src/components/SignForm.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {
+    View,
+    Text,
+    ActivityIndicator,
+    TextInput,
+} from 'react-native';
+import {
+    loginUser,
+} from '../actions/index';
+import SubmitButton from './SubmitButton';
+
+const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        margin: 10,
+    },
+    textInput: {
+        height: 40,
+        borderColor: 'grey',
+        borderWidth: 1,
+        marginTop: 10,
+    },
+};
+
+export class SignForm extends Component {
+    static propTypes = {
+        formType: PropTypes.oneOf(['login', 'register']).isRequired,
+    };
+
+    state = {
+        email: '',
+        password: '',
+    };
+
+    onEmailChange = (email) => {
+        this.setState({
+            email,
+        });
+    };
+
+    onPasswordChange = (password) => {
+        this.setState({
+            password,
+        });
+    };
+
+    onButtonPress = () => {
+        const {
+            email,
+            password,
+        } = this.state;
+
+        if (this.props.formType === 'login') {
+            this.props.loginUser({
+                email,
+                password,
+            });
+        }
+    };
+
+    renderButton = () => {
+        if (this.props.loading) {
+            return (
+                <View>
+                    <ActivityIndicator
+                        size="large"
+                    />
+                </View>
+            );
+        }
+
+        return (
+            <SubmitButton
+                onPress={this.onButtonPress}
+                submitText={this.props.formType === 'login' ? 'Se connecter' : 'S\'inscrire'}
+            />
+        );
+    };
+
+    renderTitle = () => {
+        if (this.props.formType === 'login') {
+            return 'Connexion';
+        }
+        return 'S\'enregistrer';
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{ alignSelf: 'center' }}>
+                    {this.renderTitle()}
+                </Text>
+                <View>
+                    <TextInput
+                        style={styles.textInput}
+                        onChangeText={this.onEmailChange}
+                        placeholder="Email"
+                        value={this.state.email}
+                    />
+
+                    <TextInput
+                        style={styles.textInput}
+                        secureTextEntry
+                        placeholder="Password"
+                        onChangeText={this.onPasswordChange}
+                        value={this.state.password}
+                    />
+                </View>
+
+                <Text>
+                    {this.props.error}
+                </Text>
+                {this.renderButton()}
+            </View>
+
+        );
+    }
+}
+
+const mapStateToProps = ({ auth }) => {
+    const { error, loading } = auth;
+    return { error, loading };
+};
+
+export default connect(mapStateToProps, {
+    loginUser,
+})(SignForm);
