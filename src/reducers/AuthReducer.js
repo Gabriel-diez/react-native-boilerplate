66c42ff2751a304363de430d0dc557689f8aa374
Diez Gabriel <diez.gabriel64@gmail.com>
2017-12-08 15:57:14 +0100

    add type checking for AuthReducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,3 +1,4 @@
+// @flow
 import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
@@ -7,27 +8,33 @@ import {
     CREATE_USER_SUCCESS,
 } from '../actions/types';
 
+type State = {
+    user: Object | null,
+    error: string,
+    loading: boolean
+};
+
 const INITIAL_STATE = {
     user: null,
     error: '',
     loading: false,
 };
 
-export default (state = INITIAL_STATE, action) => {
+export default (state: State = INITIAL_STATE, action: Object): State => {
     switch (action.type) {
-        case LOGIN_USER:
-            return { ...state, loading: true, error: '' };
-        case LOGIN_SUCCESS:
-            return { user: action.payload, loading: false };
-        case LOGIN_FAIL:
-            return { ...state, error: 'Identifiants incorrects', loading: false };
-        case CREATE_USER:
-            return { ...state, loading: true, error: '' };
-        case CREATE_USER_SUCCESS:
-            return { ...state, loading: false };
-        case CREATE_USER_FAIL:
-            return { ...state, error: action.payload, loading: false };
-        default:
-            return state;
+    case LOGIN_USER:
+        return { ...state, loading: true, error: '' };
+    case LOGIN_SUCCESS:
+        return { ...state, user: action.payload, loading: false };
+    case LOGIN_FAIL:
+        return { ...state, error: 'Identifiants incorrects', loading: false };
+    case CREATE_USER:
+        return { ...state, loading: true, error: '' };
+    case CREATE_USER_SUCCESS:
+        return { ...state, loading: false };
+    case CREATE_USER_FAIL:
+        return { ...state, error: action.payload, loading: false };
+    default:
+        return state;
     }
 };
